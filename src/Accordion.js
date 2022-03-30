16a68dfcf0a8a0fb72cf321815f0c2b09db3e651
Karolina <kmieczkowska75@gmail.com>
2022-03-30 22:03:08 +0100

    Add state

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import Chevron from './Chevron';
 
 const Accordion = () => {
+    const [activeState, setActiveState] = useState();
+    const itemClasses = !activeState ? 'accordion__item' : 'accordion__item accordion__item--active';
+    const handleClick = (e) => {
+        // setActiveState(true);
+        const clicked = e.target.closest('.accordion__item');
+        clicked.classList.contains('accordion__item--active') ? setActiveState(false) : setActiveState(true);
+    }
+
     return (
         <div className="accordion">
-            <div className="accordion__item">
-                <button className="accordion__btn">
+            <div className={itemClasses}>
+                <button className="accordion__btn" onClick={handleClick}>
                     Accordion Button
                     <Chevron/>
                 </button>
